c166cf87dd845333a7d2162c1fc9129c3ac22a1b
MMike17 <matthews.r.mike@gmail.com>
2026-06-25 23:20:52 +0200

    Set game flow structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,57 @@
 #include "bn_core.h"
+#include "bn_keypad.h"
+// #include "bn_common_fixed"
+
+// sprite imports
+#include "bn_sprite_items_paddle.h"
+
+enum gameState : int
+{
+	Intro,
+	Game,
+	Result
+};
+
+gameState state;
+bool inIntro;
 
 int main()
 {
-    bn::core::init();
+	Init();
 
-    while(true)
-    {
-        bn::core::update();
-    }
+	while (true)
+	{
+		Update();
+		bn::core::update();
+	}
+}
+
+void Init()
+{
+	state = gameState::Intro;
+	inIntro = false;
+
+	bn::core::init();
+}
+
+void Update()
+{
+	// how do I setup text screens for intro and outro ?
+
+	switch (state)
+	{
+	case gameState::Intro:
+		// display text here
+		if (bn::keypad::held(bn::keypad::key_type::A))
+			state = gameState::Game;
+		break;
+
+	case gameState::Game:
+		// game loop here
+		break;
+
+	case gameState::Result:
+		// display results and reset on press A
+		break;
+	}
 }
