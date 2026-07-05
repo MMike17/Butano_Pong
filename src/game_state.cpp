a29573cc3b7274d257e2cd169f705f6e1cc8d2f7
MMike17 <matthews.r.mike@gmail.com>
2026-07-05 08:43:19 +0200

    Restructure game state

--- src/game_state.cpp
+++ src/game_state.cpp
@@ -1,6 +1,6 @@
 #include "game_state.h"
+#include "bn_string_view.h"
+#include "bn_array.h"
 
-int get_gamestate_size()
-{
-	return sizeof(GameStateStrings) / sizeof(*GameStateStrings);
-}
+const int GAMESTATE_SIZE = 3;
+const bn::array<bn::string_view, 3> GameStateStrings[]{"Intro", "Game", "Result"};
